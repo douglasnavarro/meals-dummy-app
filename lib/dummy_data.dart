1f7273cea4cf7145a9e3fb01b0cfc30755c00489
Douglas Navarro <douglas.navarro@nubank.com.br>
2019-12-25 10:36:50 -0300

    add first screen with grid layout

---
+++ lib/dummy_data.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import './models/category.dart';
+
+const DUMMY_CATEGORIES = const [
+  Category(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Category(
+    id: 'c2',
+    title: 'Japanese',
+    color: Colors.pink,
+  ),
+  Category(
+    id: 'c3',
+    title: 'Brazilian',
+    color: Colors.green,
+  ),
+  Category(
+    id: 'c4',
+    title: 'Quick & Easy',
+    color: Colors.blue,
+  ),
+  Category(
+    id: 'c5',
+    title: 'French',
+    color: Colors.red,
+  ),
+];
---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:meals/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text("meals"),
+      ),
+    );
+  }
+}
